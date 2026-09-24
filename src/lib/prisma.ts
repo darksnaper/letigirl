6504b18ab52a6fc1a26536fd914d0f84980bc6ec
Matvey Komarnitskiy <darksnaper@MacBook-Air.local>
2026-09-25 01:23:34 +0300

    Use POSTGRES_PRISMA_URL and POSTGRES_URL_NON_POOLING for Vercel Postgres

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,21 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const dbUrl =
+  process.env.POSTGRES_PRISMA_URL ||
+  process.env.DATABASE_URL ||
+  process.env.POSTGRES_URL;
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    datasources: dbUrl
+      ? {
+          db: {
+            url: dbUrl,
+          },
+        }
+      : undefined,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   });
 
